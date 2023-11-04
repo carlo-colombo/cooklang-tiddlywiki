0c3e86fdb9206da78c6caf4442a5c915f8ca41c4
Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
2023-11-04 10:41:02 +0000

    ci: version bump to 0.1.3

--- scripts/setPluginVersion.mjs
+++ scripts/setPluginVersion.mjs
@@ -4,6 +4,12 @@ const path = './plugins/litapp/cooklang/plugin.info'
 
 const pluginInfo = JSON.parse(fs.readFileSync(path))
 
+console.log(process.argv)
+
+if (process.argv[2]==""){
+    process.exit(1)
+}
+
 pluginInfo.version = process.argv[2]
 
 fs.writeFileSync(path, JSON.stringify(pluginInfo, ' ', 4))
